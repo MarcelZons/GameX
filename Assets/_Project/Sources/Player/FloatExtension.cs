4f48fd92c214ba0eeed2de358e10d2b5686ec98b
Md Risul Karim <r_karim@wargaming.net>
2020-06-12 15:06:39 +0200

    [PlayerShipContrller]  Added Ship Control movement

---
+++ Assets/_Project/Sources/Player/FloatExtension.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace gamex.util
+{
+    public static class FloatExtension
+    {
+        public static float GetSmoothDamping(this float current, float target, float dampSpeed)
+        {
+            current = Mathf.Lerp(current,target , Time.deltaTime * dampSpeed);
+            return current;
+        }
+
+        public static float Clamp(this float current, float min,  float max)
+        {
+            return Mathf.Clamp(current, min, max);
+        }
+    }
+}
--- Assets/_Project/Sources/Player/PlayerShipController.cs
+++ Assets/_Project/Sources/Player/PlayerShipController.cs
@@ -1,18 +1,67 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using gamex.util;
 using UnityEngine;
 
-public class PlayerShipController : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace gamex.player
+{
+    [RequireComponent(typeof(PlayerInputController) , typeof(PlayerShipAnimationController) )]
+    public class PlayerShipController : MonoBehaviour
     {
+        [Header("Transforms")]
+        [SerializeField] private Transform _shipTransform;
+
+        [Space(10)] 
+        [Header("Movement")] 
+        [Range(1,100f)]
+        [SerializeField] private float _minSpeed;
+        [Range(1,100f)]
+        [SerializeField] private float _maxSpeed;
+        [Range(1,10f)]
+        [SerializeField] private float _accelaration;
         
+        [Space(20)]
+        [Header("Damping Values: (Low to High) = (Slower to Agile)")]
+        [Range(1f,5f)]
+        [SerializeField] private float _horizontalDamping;
+        [Range(1f,10f)]
+        [SerializeField] private float _verticalDamping;
+
+
+        private PlayerInputController _playerInputController;
+        private PlayerShipAnimationController _animationController;
+
+        private float _horizontalValue;
+        private float _verticalValue;
+        private float _forwardSpeed;
+        private float _lerpedForwardMovementSpeed;
+
+        private void Awake()
+        {
+            _playerInputController = GetComponent<PlayerInputController>();
+            _animationController = GetComponent<PlayerShipAnimationController>();
+        }
+
+        private void Start()
+        {
+            _lerpedForwardMovementSpeed = _minSpeed;
+        }
+        
+        private void Update()
+        {
+            _horizontalValue = _horizontalValue.GetSmoothDamping(_playerInputController.Horizontal,_horizontalDamping);
+            _verticalValue = _verticalValue.GetSmoothDamping(_playerInputController.Vertical, _verticalDamping);
+            
+            // Rotation
+            _shipTransform.Rotate(Vector3.up * _horizontalValue);
+            
+            // Forward Movement
+            _lerpedForwardMovementSpeed += _playerInputController.MouseScroll;
+            _lerpedForwardMovementSpeed = _lerpedForwardMovementSpeed.Clamp(_minSpeed, _maxSpeed);
+            _forwardSpeed = _forwardSpeed.GetSmoothDamping(_lerpedForwardMovementSpeed, _accelaration);
+            _shipTransform.Translate((Vector3.forward * _forwardSpeed) +  (Vector3.up * _verticalValue));
+            
+            // Animation Update
+            _animationController.UpdateAnimator(_horizontalValue, _verticalValue);   
+        }
     }
 }
