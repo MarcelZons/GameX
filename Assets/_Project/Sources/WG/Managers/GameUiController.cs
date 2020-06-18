248ab32db781dc4f2f0ffaaa80c36aef10a8cea8
Md Risul Karim <r_karim@wargaming.net>
2020-06-18 05:32:05 +0200

    Message box animation implemented

--- Assets/_Project/Sources/WG/Managers/GameUiController.cs
+++ Assets/_Project/Sources/WG/Managers/GameUiController.cs
@@ -6,14 +6,15 @@ namespace WG.GameX.Managers
 {
     public class GameUiController : MonoBehaviour
     {
-        [SerializeField] private PlayerHud _speedHud;
-        [SerializeField] private PlayerHud _energyHud;
         [SerializeField] private TextMeshProUGUI _informationText;
+        [SerializeField] private Animator _messageBoxAnimator;
         private TMP_Typewriter _inforamtionTextTypewriter;
+        private static readonly int Appear = Animator.StringToHash("Appear");
 
         private void Awake()
         {
             _inforamtionTextTypewriter = _informationText.gameObject.GetComponent<TMP_Typewriter>();
+            _informationText.text = "";
         }
 
         public void SetSpeedHud(float value)
@@ -29,13 +30,15 @@ namespace WG.GameX.Managers
         public void SetInformationText(string info)
         {
             CancelInvoke(nameof(HideInformationText));
-            _inforamtionTextTypewriter.Play(info, 50, null);
+            _messageBoxAnimator.SetBool(Appear,true);
+            _inforamtionTextTypewriter.Play(info.ToUpper(), 50, null);
             Invoke(nameof(HideInformationText), 3f);
         }
 
         private void HideInformationText()
         {
             _informationText.text = "";
+            _messageBoxAnimator.SetBool(Appear,false);
         }
 
         public void SetHealth(float healthFactor)
